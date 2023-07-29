551a9651ef4e8bb74dfeed2adf03808a056c992b
Samuel Ortion <samuel+git@ortion.fr>
2023-07-29 11:14:03 +0200

    feat: Plot English global ratio evolution

---
+++ src/english_prevalence.jl
@@ -0,0 +1,43 @@
+"""
+Compute the rate of theses written in English accross the years.
+"""
+
+using DataFrames
+using CSV
+using Dates
+
+input_csv = "data/theses-soutenues.csv"
+
+start_year = 1985
+end_year = 2022
+
+
+counter_english = Array{Int64,1}(undef, end_year - start_year + 1)
+counter_french = Array{Int64,1}(undef, end_year - start_year + 1)
+counter_missing = Array{Int64,1}(undef, end_year - start_year + 1)
+
+# Read each row of the csv without loading the whole file in memory
+for row in CSV.File(input_csv)
+    # Get the year
+    date = row.date_soutenance
+    year = Dates.year(date)
+    # Get the year index
+    if year < start_year || year > end_year
+        return
+    end
+    index = year - start_year + 1
+    # Increment the counter
+    if ismissing(row.langue)
+        counter_missing[index] += 1
+    else 
+        if row.langue == "en"
+            counter_english[index] += 1
+        elseif row.langue == "fr"
+            counter_french[index] += 1
+        end
+    end
+end
+
+# Write the distribution to a CSV file
+output_csv = "tmp/english_prevalence.csv"
+CSV.write(output_csv, DataFrame(year=start_year:end_year, english=counter_english, french=counter_french, missing=counter_missing))
--- src/year_distribution.jl
+++ src/year_distribution.jl
@@ -8,29 +8,25 @@ using Dates
 
 input_csv = "tmp/theses-soutenues-filtered.csv"
 
-# Read the CSV file into a DataFrame
-df = DataFrame(CSV.File(input_csv))
-
-
 # Compute the distribution of years
-start_year = 2000
-end_year = 2023
+start_year = 1985
+end_year = 2022
 
 year_counter = Array{Int64,1}(undef, end_year - start_year + 1)
 
 # For each thesis
-foreach(row -> begin
-        # Get the year
-        date = row[:date_soutenance]
-        year = Dates.year(date)
-        # Get the year index
-        if year < start_year || year > end_year
-            return
-        end
-        index = year - start_year + 1
-        # Increment the counter
-        year_counter[index] += 1
-    end, eachrow(df))
+for row in CSV.File(input_csv)
+    # Get the year
+    date = row.date_soutenance
+    year = Dates.year(date)
+    # Get the year index
+    if year < start_year || year > end_year
+        return
+    end
+    index = year - start_year + 1
+    # Increment the counter
+    year_counter[index] += 1
+end
 
 # Write the distribution to a CSV file
 output_csv = "tmp/year_distribution.csv"
